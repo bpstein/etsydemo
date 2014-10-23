ca83df676c2d1d7f706f7208eb0ef80bcfbb754a
Ben Stein <bumstain@gmail.com>
2014-10-24 00:18:05 +0100

    Updated handling of recipients and posting multiple listings

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -42,10 +42,12 @@ class ListingsController < ApplicationController
         :type => "individual",
         :bank_account => token
         )
+      
+      current_user.recipient = recipient.id
+      current_user.save
     end
 
-    current_user.recipient = recipient.id
-    current_user.save
+    
 
     respond_to do |format|
       if @listing.save
